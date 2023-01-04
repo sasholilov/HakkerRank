b6b97dd92b70b6e8cfd7a812bab2d652587afb1e
sasholilov <alex_xld@abv.bg>
2023-01-04 21:56:37 +0200

    CamelCase4

---
+++ CamelCase4/CamelCase4.js
@@ -0,0 +1,37 @@
+'use strict';
+function processData(input) {
+  for (const b of input) {
+    if (b.startsWith('S;M;')) {
+      const strArr = b.slice(4, -2).split(/(?=[A-Z])/);
+      console.log(strArr[0] + ' ' + strArr[1].slice(0).toLowerCase());
+    }
+  }
+  for (const b of input) {
+    if (b.startsWith('S;V;')) {
+      const strArr = b.slice(4).split(/(?=[A-Z])/);
+      console.log(strArr[0] + ' ' + strArr[1].slice(0).toLowerCase());
+    }
+  }
+  for (const b of input) {
+    if (b.startsWith('C;M;')) {
+      const strArr = b.slice(4).split(' ');
+      const strArrCorrect = [];
+      for (let i = 0; i < strArr.length; i++) {
+        strArrCorrect.push(
+          strArr[i].slice(0, 1).toUpperCase() + strArr[i].slice(1)
+        );
+      }
+      console.log(strArrCorrect.join(''));
+    }
+  }
+}
+
+const input = [
+  'S;M;plasticCup()',
+  'C;V;mobile phone',
+  'C;C;coffee machine',
+  'S;C;LargeSoftwareBook',
+  'C;M;white sheet of paper',
+  'S;V;iPad',
+];
+processData(input);
