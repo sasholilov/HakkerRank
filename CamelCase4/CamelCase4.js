cef16b0cd838d1b3c1b999b72c09382029847491
sasholilov <alex_xld@abv.bg>
2023-01-04 22:23:05 +0200

    Update CamelCase4.js

--- CamelCase4/CamelCase4.js
+++ CamelCase4/CamelCase4.js
@@ -9,7 +9,13 @@ function processData(input) {
   for (const b of input) {
     if (b.startsWith('S;V;')) {
       const strArr = b.slice(4).split(/(?=[A-Z])/);
-      console.log(strArr[0] + ' ' + strArr[1].slice(0).toLowerCase());
+      const strArrCorrect = [];
+      for (let i = 0; i < strArr.length; i++) {
+        strArrCorrect.push(
+          strArr[i].slice(0, 1).toLowerCase() + strArr[i].slice(1)
+        );
+      }
+      console.log(strArrCorrect.join(' '));
     }
   }
   for (const b of input) {
@@ -33,5 +39,7 @@ const input = [
   'S;C;LargeSoftwareBook',
   'C;M;white sheet of paper',
   'S;V;iPad',
+  'S;V;iPhoneDevice',
+  'S;V;newVeriableConst',
 ];
 processData(input);
